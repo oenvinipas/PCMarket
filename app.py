e99b3db7845c82d424a9f047dc7d647b829a1fc5
jfoste81 <jfoste81@uncc.edu>
2023-12-14 17:15:49 -0500

    Changing app config

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
+# app.config[
+#     "SQLALCHEMY_DATABASE_URI"
+# ] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")   
 
 app.secret_key = os.getenv("DB_SECRET_KEY", "potato")
 app.permanent_session_lifetime = timedelta(minutes=30)
